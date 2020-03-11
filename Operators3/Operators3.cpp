433ce357d287f4fa38cfbdcf8f58b60e52d7b2e8
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-03-12 00:40:58 +0100

    Chapters 5.7 - 5.x

---
+++ Operators3/Operators3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int main()
+{
+	/*//5.7 Q1
+	(true && true) || false
+	true
+
+	(false && true) || true
+	true
+
+	(false && true) || false || true
+	true
+
+	(5 > 6 || 4 > 3) && (7 > 8)
+	false
+
+	!(7 > 6 || 3 > 4)
+	false
+	*/
+
+	//5.x
+	/*//Q1
+	(5 > 3 && 4 < 8)
+	true
+
+	(4 > 6 && true)
+	false
+
+	(3 >= 3 || false)
+	true
+
+	(true || false) ? 4 : 5
+	4
+
+	*/
+
+	/*//Q2
+	7 / 4
+	1
+
+	14 % 5
+	4
+	*/
+}
