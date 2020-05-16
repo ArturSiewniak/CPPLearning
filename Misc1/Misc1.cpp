733ac820b56c0dfc2a7c48eac207d21098a26e7e
Artur Siewniak <43797135+ArturSiewniak@users.noreply.github.com>
2020-05-16 23:27:29 +0200

    Chapters A.1 - C.1

---
+++ Misc1/Misc1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "curses.h"
+#pragma(comment lib, "PDCurses.lib")
+
+int main()
+{
+	//Chapters A.1 - A.3; B.1 - B.3; C.1 - nothing new besides library inclusion
+
+	return 0;
+}
